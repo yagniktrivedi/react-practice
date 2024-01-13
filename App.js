f3b72c9ddb17b545044683baca8b793f81db7e03
yagniktrivedi <yagnikvyagnik@gmail.com>
2024-01-13 12:40:07 +0530

    package.json

---
+++ App.js
@@ -0,0 +1,28 @@
+/*
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyx: "test params" },
+  "Hello World from React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log("root", root);
+
+root.render(heading);
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1"),
+    React.createElement("h1", {}, "I am h1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 of second child"),
+    React.createElement("h1", {}, "I am h1 of second child"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
